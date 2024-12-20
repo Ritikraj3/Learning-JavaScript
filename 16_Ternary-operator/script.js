571183e4044d9e0fd1bd0baae16477cc972c7536
Ritikraj3 <ritikraj5776@gmail.com>
2024-12-20 16:33:07 +0530

    ternary basiccd

---
+++ 16_Ternary-operator/script.js
@@ -0,0 +1,12 @@
+// debugger
+// const username = 5 > 2 ? 'Ritik' : 'Developer'
+
+const gender = 'F'
+
+// debugger
+// const userMessage = `${ gender === 'F'  ? 'She' : 'He'} is a college student.`
+// console.log(userMessage);
+// console.log(`${useRMessage} is a college student`);
+const result = null ? 'Ritik' : '' ? '12' : 0
+
+console.log(result);

3d3ec69433d3e5b92ee1d9c921cd88ecfccbb362
Ritikraj3 <ritikraj5776@gmail.com>
2024-12-28 19:28:21 +0530

    object in JS

---
+++ 18_Objects-in-JS/Script.js
@@ -0,0 +1,43 @@
+const myName = 'Ritik'
+
+const username1 = ""
+const username2 = ""
+
+const user1 = {
+  firstName: "Ritik",
+  lastName: "raj",
+//   age: 24,
+//   education: 12,
+//   city: "Bangalore",
+};
+
+// const user2 = {
+//     firstName: "Rishabh",
+//     'last-Name': "raj",
+//     ritik: 'Developer'
+//     // age: 24,
+//     // education: 12,
+//     // city: "Bangalore",
+//   };
+
+//   console.log(user1.firstName);
+//   console.log(user2["last-Name"]);
+//   console.log(user2.ritik);
+//   console.log(user2[myName]);
+//   console.log(user2['first' + 'Name']);
+
+const user2 = {
+    firstName: "Rishabh",
+    lastName: "raj",
+    address:{
+        city: 'Bangalore',
+        pinCode: '560037',
+        state: 'Karnataka',
+        moreDetails: {
+            expert: 'JavaScript',
+            phoneNo: 9304754028
+        }
+    }
+  };
+//   user2.age = 24
+//   user2['is-student'] = true

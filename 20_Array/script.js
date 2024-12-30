15835b34d13ff11ce32ee41d5ba81393f9d5feb7
Ritikraj3 <ritikraj5776@gmail.com>
2024-12-31 03:41:22 +0530

    array ij JS

---
+++ 20_Array/script.js
@@ -0,0 +1,8 @@
+const userName = 'ritik raj'
+const fruitsList = ['Apple' , 'Banana' , 'Grapes' , 'Dates' ] 
+
+// const rainbowColors =  ['violet' , 'indigo' , 'blue' , 'green' , 'yellow' , 'orange' , 'red']
+
+const newObject = {}
+
+const newArray = []

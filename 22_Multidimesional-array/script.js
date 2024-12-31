93bb50a1d59e624b35c7d8ae79c5b944a20be6ed
Ritikraj3 <ritikraj5776@gmail.com>
2024-12-31 18:32:50 +0530

    multidimensional array

---
+++ 22_Multidimesional-array/script.js
@@ -0,0 +1,11 @@
+const nameAndNumberList = [
+  ["Adarsh", 85],
+  ["Akash", 70],
+  ["Ritik", 99],
+];
+
+const ticTacToecolumn1 = [
+    ['x' , null , null],
+    [null , null , 'o'],
+    ['o' , null , 'x']
+]

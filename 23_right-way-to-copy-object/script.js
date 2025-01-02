4d1beaad0bd27a75eb88cb9639a097d650509fa7
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-02 18:42:53 +0530

    DEEP COPY - SHALLOW COPY

---
+++ 23_right-way-to-copy-object/script.js
@@ -0,0 +1,47 @@
+// const userName1 = 'Ritik'
+// let userName2 = userName1
+// userName2 = userName2 + 'Raj'
+
+const fruits = ['Mango', 'Apple', 'Orange']
+
+// const myFruits = fruits
+
+// myFruits.push('Grapes')
+// myFruits.push('dates')
+
+// const myFruits = []
+// Object.assign(myFruits, fruits )
+
+// const myFruit = [...fruits]
+
+// const myFruits = [].concat(fruits)
+// const myFruits = fruits.slice()
+
+
+
+const user1 = {
+    firstName: 'Ritik',
+    lastName: 'Raj',
+    pata:{
+        city:'Bengaluru',
+        pincode: 560037,
+    },
+    subject: ['Physics', 'CS', 'Maths']
+}
+
+// /DEEP COPY
+// const user2 = structuredClone(user1)
+// const user2 = JSON.parse(JSON.stringify(user1))
+
+
+
+// const user2 = user1
+// user2.lastName = 'Karn'
+// user1.firstName = 'Rishabh'
+
+
+// SHALLOW COPY
+// const user2 = {}
+// Object.assign(user2, user1)
+//  const user2 = {...user1}
+

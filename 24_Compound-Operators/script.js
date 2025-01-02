dbdbf615b7766f08e1c4c34425162cd5c5b944fe
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-03 01:03:15 +0530

    Compound operators

---
+++ 24_Compound-Operators/script.js
@@ -0,0 +1,31 @@
+const userName = 'Ritik'
+
+let num = 5
+
+// debugger
+// num = num + 2
+// num = num + 2
+// num = num + 2
+// num = num + 2
+
+// num += 5
+// num += 5
+// num += 5
+// num += 5
+// num += 5
+
+// num = num * 2
+// num = num * 2
+// num = num * 2
+
+// num *= 2
+// num *= 2
+// num *= 2
+
+// const newNum = ++num
+// const newNum = --num
+
+// const newNum = num++
+// const newNum = num--
+
+num++

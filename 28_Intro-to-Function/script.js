bdd05ba0536c0c24a6421673d1947364153f2580
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-08 11:10:08 +0530

    function 01

---
+++ 28_Intro-to-Function/script.js
@@ -0,0 +1,18 @@
+// console.log('Hi,');
+// console.log('My name is Ritik Raj.');
+// console.log('I am a Web Developer');
+
+//Function Definition
+function introduceMe(username,job,age) {
+    console.log('Hi,');
+console.log(`My name is ${username}.`);
+console.log(`I am a ${job}.`);
+console.log(`i am ${age} years old.`);
+}
+
+//                 Function Call
+// const returnValue = introduceMe()
+
+introduceMe('Rishabh', 'Data Analyst', 25)
+introduceMe('Ritik', 'Web Developer', 27)
+

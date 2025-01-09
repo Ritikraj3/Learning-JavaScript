ceb179ac6fb9ce2832e2c1b779af790678946041
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-09 22:23:28 +0530

    execution contest

---
+++ 30_execution-contest/script.js
@@ -0,0 +1,22 @@
+debugger
+
+
+sayhi()
+
+
+var username = "Ritik"
+var userAge = 25
+
+
+function sayhi() {
+
+    const a = 14
+    const b = 12
+
+    console.log(a , b);
+}
+
+console.log('Program Ended');
+
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName

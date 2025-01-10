d515c2d710b8eac110f35faf4c5091f1cc772a24
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-10 18:18:26 +0530

    global scope vs local scope

---
+++ 33_Scope-in-JS/script.js
@@ -0,0 +1,26 @@
+debugger
+
+const username = 'Ritik'
+let userAge = 24
+var a = 50
+
+function add() {
+    const x = 5
+    const y = 8
+    console.log(x + y)
+    console.log(username)
+}
+
+
+function subtract() {
+    const x = 5
+    const y = 8
+    console.log(x - y)
+    console.log(username)
+}
+
+add()
+
+subtract()
+
+console.log('Program Ended')

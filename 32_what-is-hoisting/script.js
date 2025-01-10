8060b80262d4988cc577078e398191a5f39d1565
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-10 15:44:35 +0530

    what is hoisting

---
+++ 32_what-is-hoisting/script.js
@@ -0,0 +1,26 @@
+debugger
+
+console.log(a);
+
+var a = 'Ritik'
+
+hii()
+
+
+//function definition
+//function Declaration
+function hii() {
+    console.log('Hello World');
+}
+
+
+
+//function definition
+//function Expression
+var sayHi = function hi() {           //anonymous function
+    console.log('Hiii');
+}
+
+//IIFE   
+
+sayHi()

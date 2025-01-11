77bfd20ed11b7ea07d0e8686b678f6487837114f
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-11 18:39:35 +0530

    laxical and global scope

---
+++ 34_laxical-and-block-scope/script.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const username = 'Ritik'
+let userAge = 25
+var a = 50
+
+// function add() {
+//   debugger
+//   const username = 'Akash'
+//   const x = 5
+//   const y = 8
+//   console.log(x + y)
+//   console.log(username)
+// }
+
+function subtract() {
+    // debugger
+  const x = 15
+  const y = 18
+  const z = 17
+  console.log(x - y)
+  console.log(username)
+
+  function child() {
+    // debugger
+    const childName = 'Rohit'
+    console.log(childName);
+    console.log(x , z);
+
+   if(true) {
+        let num1 = 101
+        var num2 = 202
+        console.log(num1);
+        console.log(num2);
+    }
+
+    console.log(num2);
+
+    
+
+    function grandChild() {
+        const grandChildName = 'molu'
+        console.log(grandChildName);
+    }
+
+    grandChild()
+  }
+
+
+  child() 
+
+  console.log(num2);
+
+  
+}
+
+
+
+
+subtract()
+
+
+// add()
+
+
+console.log('Program Ended')

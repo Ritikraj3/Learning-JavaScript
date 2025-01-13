013c1b3057b250b228d01f0dd47ca9a5af15c03e
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-13 16:09:09 +0530

    setTimeout and setInterval

---
+++ 36_setTimeout-setInterval/script.js
@@ -0,0 +1,27 @@
+// const timer1 = setTimeout(`console.log("hy-1")`, 1000)
+// const timer2 = setTimeout(`console.log("hy-2")`, 2000)
+// const timer3 = setTimeout(a, 3000, 'rfaf', 58, 44)
+// const timer1 = setInterval(`console.log("hy-1")`, 1000)
+// const timer2 = setInterval(`console.log("hy-2")`, 2000)
+const timer3 = setInterval(a, 3000, 'rfaf', 58, 44)
+
+// clearTimeout(timer2)
+
+function a() {
+    // console.log(arguments[2]);
+    console.log('HelloWorld');
+}
+
+// console.log('ugesvyucv');
+
+
+
+//anonymous function
+
+// setTimeout(function() {
+//     console.log('Hi-1');
+// }, 0)
+
+// console.log('Hi-2');
+
+

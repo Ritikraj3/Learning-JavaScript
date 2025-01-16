2dd8b3a65ac9f2f8aa39d1911c05f050344c004a
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-16 19:20:08 +0530

    event loop and callback

---
+++ 37_Event-loop-and-callback-qeue/script.js
@@ -0,0 +1,16 @@
+console.log('Hi-1');
+
+
+function hello() {
+    console.log('Hello World');
+}
+
+for(let i = 0; i <=4; i++){
+    console.log(i);
+}
+ hello()
+
+
+setTimeout(`hello , 0`)
+
+console.log('Hi-2');

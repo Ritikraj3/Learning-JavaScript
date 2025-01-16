d9be5c0f6e89a4dddd890bfad27cae5316008c1b
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-16 20:18:55 +0530

    Methods and Function

---
+++ 39_Methods-and-Function/script.js
@@ -0,0 +1,17 @@
+const maths = {
+    add: function (a , b) {
+        return a + b
+    } ,
+    
+    square: function (y) {
+        return y * y
+    },
+    
+    subtract(a , b) {
+        return a - b
+    },
+
+    cube(num) {
+        return num ** 3
+    }
+}

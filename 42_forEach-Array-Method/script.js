9d487078ddfad8cc45ad2b42fdd061ddec825f45
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-17 15:00:34 +0530

    forEach method

---
+++ 42_forEach-Array-Method/script.js
@@ -0,0 +1,21 @@
+const fruits = ['banana', 'apple', 'peach', 'mango', 'grapes', 'chiku']
+
+
+// for(const fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// fruits.forEach(function(fruit) {
+//     console.log(fruit);
+// })
+
+fruits.forEach((fruit) => {
+    console.log(fruit);
+})
+
+
+// function abc(fruit) {
+//     console.log(fruit);
+// }
+
+// fruits.forEach(abc)

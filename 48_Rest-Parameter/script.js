fe2db3a1bb0ea20e82169c93933d9a3d57537da5
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-21 23:18:09 +0530

    Rest Params

--- 48_Rest-Parameter/script.js
+++ 48_Rest-Parameter/script.js
@@ -1,10 +1,28 @@
 const nums1 = [1, 2, 3, 4]
 
-function add() {
-    console.log(arguments);
-    let sum = 0
-    for(let i = 0; i < arguments.length; i++) {
-        sum = sum + arguments[i]
-    }
-    return sum
+// function add(a, b, c, ...nums) {
+//     console.log(a, b, c);
+//     console.log('nums:',  nums);
+//     let sum = 0
+//     for(let i = 0; i < nums.length; i++) {
+//         sum = sum + nums[i]
+//     }
+//     return sum
+// }
+
+
+function add(...nums) {
+    return nums.reduce((acc, curr) => acc + curr)
 }
+
+
+// function add() {
+//     return [...arguments].reduce((acc, curr) => acc + curr)
+// }
+
+
+// function add() {
+//     return Array.from(arguments).reduce((acc, curr) => acc + curr)
+// }
+
+const result = add(...nums1, 5, 10, 15)

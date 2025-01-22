866e6d9d901318db1281347ef735750fa56d963e
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-23 00:56:01 +0530

    document object model

---
+++ 51_DOM-in-JS/script.js
@@ -0,0 +1,7 @@
+function img() {
+    document.body.children[3].src =
+    "https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=600&quality=80";
+  
+}
+
+

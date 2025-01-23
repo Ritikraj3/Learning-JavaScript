0a05228f32f4bb64505607ae6b9288589d2b6f6c
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-23 18:19:19 +0530

    innerText vs textContent

---
+++ 53_innerText-vs-innerContent/script.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1')
+
+const paragraph = document.querySelector('p')
+
+// console.log(paragraph.innerHTML);
+
+console.log(paragraph.innerText);
+console.log(paragraph.textContent);

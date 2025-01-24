aaaa4edb56456a7e83b2ab443816bc327d4d4c2f
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-24 23:49:54 +0530

    sibling & element selector

--- 56_Access-parent-sibling-children-Elements/script.js
+++ 56_Access-parent-sibling-children-Elements/script.js
@@ -1,34 +1,36 @@
-const h1 = document.querySelector('h1')
-const paragraph = document.querySelector('p')
+const h1 = document.querySelector("h1");
+const paragraph = document.querySelector("p");
 
-h1.style.color = 'blue'
+h1.style.color = "blue";
 
-h1.style.backgroundColor = 'skyblue'
+h1.style.backgroundColor = "skyblue";
 
-const allAnc = document.querySelectorAll('a')
+const allAnc = document.querySelectorAll("a");
 
 // for (let i = 0; i < allAnc.length; i++) {
 //        allAnc[i].style.color = 'teal'
 //  }
 
 for (const Anc of allAnc) {
-    // Anc.style.color = 'green'
-    // Anc.style.textDecoration = 'none'
-    // Anc.style.fontWeight = '700'
-    // Anc.style.fontFamily = 'cursive'
+  // Anc.style.color = 'green'
+  // Anc.style.textDecoration = 'none'
+  // Anc.style.fontWeight = '700'
+  // Anc.style.fontFamily = 'cursive'
 
-    // Anc.style.cssText = 
-    // `color: red;
-    // font-size: 18px;
-    // font-family: cursive;
-    // font-weight: 700;
-    // text-decoration-line: none;`
-    // Anc.className = 'my-link Anc-link'
-    // Anc.setAttribute('class', 'Anc-link')
-    Anc.classList.add('Anc-link')
-    Anc.classList.remove('my-link')
-    Anc.classList.toggle('wavy-link')
-
-    
-
+  // Anc.style.cssText =
+  // `color: red;
+  // font-size: 18px;
+  // font-family: cursive;
+  // font-weight: 700;
+  // text-decoration-line: none;`
+  // Anc.className = 'my-link Anc-link'
+  // Anc.setAttribute('class', 'Anc-link')
+  Anc.classList.add("Anc-link");
+  Anc.classList.remove("my-link");
+  Anc.classList.toggle("wavy-link");
 }
+
+const firstLink = document.querySelector(
+  "body > p:nth-child(5) > a:nth-child(2)"
+);
+

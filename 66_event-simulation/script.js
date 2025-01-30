8b33155025cd0492def70b0e438dd55dbe1d15a8
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-30 18:48:52 +0530

    event simulator

--- 66_event-simulation/script.js
+++ 66_event-simulation/script.js
@@ -1,5 +1,7 @@
 const addCardBtn = document.querySelector('.card')
 const container = document.querySelector('.container')
+const input = document.querySelector('input')
+const form = document.querySelector('form')
 
 let count = 1
 
@@ -9,3 +11,31 @@ addCardBtn.addEventListener('click', () => {
     newCard.innerText = count++
     container.append(newCard)
 })
+
+
+//FOCUS EVENT RELATED TO INPUT
+// setTimeout(() => {
+//     input.focus()
+//     console.log('input focused');
+// }, 2000)
+
+// setTimeout(() => {
+//     input.focus()
+//     console.log('input blurred');
+// }, 4000)
+
+// setTimeout(() => {
+//     form.submit()
+//     console.log('form submitted');
+// }, 2000)
+
+// form.reset()
+
+//*********************************************************** */
+
+// const intervalId = setInterval(() => {
+//     if (count > 999) {
+//         clearInterval(intervalId)
+//     }
+//     addCardBtn.click()
+// },5)

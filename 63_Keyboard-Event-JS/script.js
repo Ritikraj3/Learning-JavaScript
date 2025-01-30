f223c6131c1af18318f570a8894576b0df2b800e
Ritikraj3 <ritikraj5776@gmail.com>
2025-01-30 16:03:25 +0530

    updated

--- 63_Keyboard-Event-JS/script.js
+++ 63_Keyboard-Event-JS/script.js
@@ -1,14 +1,14 @@
 // we can use keyboard event on any element but it should be focused
 // const h1 = document.querySelector('h1')
 
-window.addEventListener('keypress', (e) => {
-    console.log('value', e.key);
-    console.log('code', e.code);
-})
-window.addEventListener('keyup', (e) => {
-    console.log('value', e.key);
-    console.log('code', e.code);
-})
+// window.addEventListener('keypress', (e) => {
+//     console.log('value', e.key);
+//     console.log('code', e.code);
+// })
+// window.addEventListener('keyup', (e) => {
+//     console.log('value', e.key);
+//     console.log('code', e.code);
+// })
 window.addEventListener('keydown', (e) => {
     console.log('value', e.key);
     console.log('code', e.code);

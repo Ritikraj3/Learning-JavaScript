e19326616e66c7bc5e519252af8be71393662bff
Ritikraj3 <ritikraj5776@gmail.com>
2025-02-20 03:58:22 +0530

    export - import and ES6 Modules

---
+++ 79_import-export-ES6-modules/script.js
@@ -0,0 +1,19 @@
+
+// import { usersData, myName as name} from "./usersData.js";
+
+import  getFirstUser, { myName } from "./usersData.js";
+
+// import  * as something from "./usersData.js";
+
+import productsData from "./productsData.js";
+
+
+// console.log(something.usersData);
+
+console.log(getFirstUser());
+console.log(myName);
+
+console.log(productsData);
+
+
+
